a2e0e03ca8ecc22c27f22f006c41b2c9cc8403ff
rumpear <roman.f.grusha@gmail.com>
2022-07-25 18:33:15 +0300

    upd signup

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,17 +1,24 @@
+const bcrypt = require('bcryptjs');
 const { User } = require('../../models');
 const services = require('../../services');
 const { createError } = require('../../utils');
 
 const signup = async (req, res, next) => {
   try {
-    // const { email } = req.body;
-    // const user = await User.findOne({ email });
-    // if (user) {
-    //   throw createError(409, `Email ${email} in use`);
-    // }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw createError(409, `Email ${email} in use`);
+    }
 
-    const data = await services.signup(req.body, next);
-    // const data = await services.signup(req.body);
+    const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+    // const data = await services.signup(req.body, next);
+    const data = await services.signup({
+      ...req.body,
+      password: hashPass,
+    });
+
     res.status(201).json({ status: 'success', code: 200, data });
   } catch (error) {
     next(error);
--- services/auth.js
+++ services/auth.js
@@ -1,19 +1,19 @@
 const { User } = require('../models');
-const { createError } = require('../utils');
+// const { createError } = require('../utils');
 
 const signup = async (body, next) => {
   try {
-    const { email } = body;
-    const user = await User.findOne({ email });
-    if (user) {
-      throw createError(409, `Email ${email} in use`);
-    }
+    // const { email } = body;
+    // const user = await User.findOne({ email });
+    // if (user) {
+    //   throw createError(409, `Email ${email} in use`);
+    // }
 
     const data = await User.create(body);
     return data;
   } catch (error) {
     console.log(error.message);
-    next(error);
+    // next(error);
   }
 };
 
